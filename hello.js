d9f19a2a95d9366a3b5ab103a8542ea4ca0c26e4
Evgeny Mastepan <phenikomperekt@gmail.com>
2025-08-18 10:51:13 +0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,21 +1,7 @@
-{\rtf1\ansi\ansicpg1251\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red123\green143\blue171;\red25\green26\blue29;\red255\green255\blue255;
-\red220\green229\blue239;\red184\green130\blue220;\red226\green158\blue107;\red91\green169\blue224;\red129\green201\blue99;
-}
-{\*\expandedcolortbl;;\cssrgb\c55294\c63137\c72549;\cssrgb\c12941\c13725\c15294;\cssrgb\c100000\c100000\c100000;
-\cssrgb\c89020\c91765\c94902;\cssrgb\c77647\c60000\c89020;\cssrgb\c91373\c68235\c49412;\cssrgb\c42353\c72157\c90196;\cssrgb\c56863\c81569\c46275;
-}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs25\fsmilli12573 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 // \uc0\u1055 \u1088 \u1086 \u1089 \u1090 \u1086 \u1081  API-\u1101 \u1085 \u1076 \u1087 \u1086 \u1080 \u1085 \u1090 \cf4 \strokec4 \
-module\cf5 \strokec5 .\cf6 \strokec6 exports\cf4 \strokec4  \cf7 \strokec7 =\cf4 \strokec4  \cf5 \strokec5 (\cf8 \strokec8 req\cf5 \strokec5 ,\cf8 \strokec8  res\cf5 \strokec5 )\cf4 \strokec4  \cf7 \strokec7 =>\cf4 \strokec4  \cf5 \strokec5 \{\cf4 \strokec4 \
-  res\cf5 \strokec5 .\cf6 \strokec6 json\cf5 \strokec5 (\{\cf4 \strokec4  \
-    \cf8 \strokec8 message\cf7 \strokec7 :\cf4 \strokec4  \cf9 \strokec9 "\uc0\u1055 \u1088 \u1080 \u1074 \u1077 \u1090  \u1089  mastepan.ru!"\cf5 \strokec5 ,\cf4 \strokec4 \
-    \cf8 \strokec8 timestamp\cf7 \strokec7 :\cf4 \strokec4  \cf7 \strokec7 new\cf4 \strokec4  \cf8 \strokec8 Date\cf5 \strokec5 ().\cf6 \strokec6 toISOString\cf5 \strokec5 ()\cf4 \strokec4  \
-  \cf5 \strokec5 \});\cf4 \strokec4 \
-\pard\pardeftab720\partightenfactor0
-\cf5 \strokec5 \};}
+// Простой API-эндпоинт
+module.exports = (req, res) => {
+  res.json({ 
+    message: "Привет с mastepan.ru!",
+    timestamp: new Date().toISOString() 
+  });
+};
